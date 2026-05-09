5d877b7562da317a20ad0a74b702c09c366e8453
Isabelle Moreira <isamoreira280554@gmail.com>
2026-05-09 12:21:23 -0300

    Módulo 2.2 concluído

--- src/PrincipalJava.java
+++ src/PrincipalJava.java
@@ -8,9 +8,9 @@ import java.util.ArrayList;
 
 public class PrincipalJava {
     public static void main(String[] args){
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("O Poderoso Chefão");
-        meuFilme.setAnoDeLancamento(1970);
+        Filme meuFilme = new Filme("O Poderoso Chefão", 1970);
+        //meuFilme.setNome("O Poderoso Chefão");
+        //meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duração do Filme:" + meuFilme.getDuracaoEmMinutos());
 
@@ -22,17 +22,17 @@ public class PrincipalJava {
         System.out.println("Total de avaliações: "+ meuFilme.getTotalDeAvaliacoes());
         System.out.println("Média de Avaliações: "+ meuFilme.retornaMedia());
 
-        Serie lost = new Serie();
-        lost.setNome("Lost");
+        Serie lost = new Serie("Lost",2003);
+        //lost.setNome("Lost");
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
         System.out.println("Duração para maratonar lost: " + lost.getDuracaoEmMinutos());
 
-        Filme outroFilme = new Filme();
-        meuFilme.setNome("Avatar");
-        meuFilme.setAnoDeLancamento(2023);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        //euFilme.setNome("Avatar");
+        //meuFilme.setAnoDeLancamento(2023);
         meuFilme.setDuracaoEmMinutos(200);
 
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
@@ -49,9 +49,9 @@ public class PrincipalJava {
         episodio.setTotalDeVizualizacoes(300);
         filtro.filtra(episodio);
 
-        Filme filme3 = new Filme();
-        filme3.setNome("Dogville");
-        filme3.setAnoDeLancamento(2003);
+        Filme filme3 = new Filme("Dogville", 2003);
+        //filme3.setNome("Dogville");
+        //filme3.setAnoDeLancamento(2003);
         filme3.setDuracaoEmMinutos(200);
         filme3.avalia(10);
 
